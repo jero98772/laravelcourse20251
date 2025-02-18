7c19a6c7266cb328fbcf67dc1bead143c5a2fa78
Daniel Correa <yo@danielgara.com>
2025-02-18 16:02:19 -0500

    imp: login

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("i
 Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name("imagenotdi.index");
 Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name("imagenotdi.save");
 
+
+Auth::routes();
